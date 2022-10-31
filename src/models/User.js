ac69eed781127b1af58fad07037fbbfc459c89ae
Marcelo Carvalho <marcelo.carvalho2@outlook.com>
2022-10-30 23:47:07 -0300

    requirement 1/2

---
+++ src/models/User.js
@@ -0,0 +1,22 @@
+const UserModel = (sequelize, DataTypes) => {
+    const User = sequelize.define('User', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: DataTypes.INTEGER
+      },
+      displayName: DataTypes.STRING,
+      email: DataTypes.STRING,
+      password: DataTypes.STRING,
+      image: DataTypes.STRING
+    }, {
+      timestamps: false,
+      tableName: 'users',
+      underscored: true,
+    });
+  
+    return User;
+  };
+  
+  module.exports = UserModel;
