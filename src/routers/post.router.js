460386038adb5c0e83c72e80e2b3efcc3c28c64a
Marcelo Carvalho <marcelo.carvalho2@outlook.com>
2022-11-03 12:39:39 -0300

    requirement 13 again

--- src/routers/post.router.js
+++ src/routers/post.router.js
@@ -3,6 +3,7 @@ const { getPosts } = require('../controllers/post.controller');
 const { getToken } = require('../middlewares/error.tokenValidations');
 
 const router = express.Router();
+
 router.use(getToken);
 
 router.get('/', getPosts);
